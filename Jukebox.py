8c06e5924231b00ba938c72265b0d4977d45b6e0
Peter Steffensen <psteffensen@gmail.com>
2019-03-16 12:57:14 +0100

    df

--- Jukebox.py
+++ Jukebox.py
@@ -205,14 +205,14 @@ class Page():
             images.append(pygame.image.load('images/cirkeline_small_1.png'))
             images.append(pygame.image.load('images/cirkeline_small_2.png'))
             music = 'music/Cirkeline - Lossepladsen.mp3'
-            pos = [0,160,130,160]
+            pos = [0,100,130,160]
             cirkeline_sprite = AnimateSprite(images, music, pos)
             
             # Mamma Mia
             images = []
             images.append(pygame.image.load('images/abba - mamma mia.png'))
             music = 'music/ABBA - Mamma Mia.mp3'
-            pos = [130,160,130,160]
+            pos = [160,160,130,160]
             mammamia_sprite = RotateSprite(images, music, pos)
 
             self.sprite_group.add(mammamia_sprite)
